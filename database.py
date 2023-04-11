6ebcf9515cf2a7bb318d6b87d0f1fd686e4da18b
Niharika3333 <63079773+Niharika3333@users.noreply.github.com>
2023-04-11 18:46:57 +0000

    store applications in database.

--- database.py
+++ database.py
@@ -19,8 +19,6 @@ engine = create_engine(
   })
 
 
-
-
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("SELECT * FROM jobs"))
@@ -29,15 +27,51 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
     return jobs
 
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text(f"SELECT * FROM jobs WHERE id={id}"))
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
       return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    job_id = job_id
+    full_name = data['full_name']
+    email = data['email']
+    linkedin_url = data['linkedin_url']
+    education = data['education']
+    work_experience = data['work_experience']
+    resume_url = data['resume_url']
+    
+    query = text(
+      f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES('{job_id}', '{full_name}', '{email}', '{linkedin_url}', '{education}', '{work_experience}', '{resume_url}')")
+
+    conn.execute(query)
+
+       # query = text(
+    #   "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES(:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    # conn.execute(query,
+    #              job_id=job_id,
+    #              full_name=data['full_name'],
+    #              email=data['email'],
+    #              linkedin_url=data['linkedin_url'],
+    #              education=data['education'],
+    #              work_experience=data['work_experience'],
+    #              resume_url=data['resume_url'])
+
+
+
+
+
+
+
+
 
 
 
@@ -50,40 +84,30 @@ def load_job_from_db(id):
 
 # with engine.connect() as conn:
 #   result = conn.execute(text("select * from jobs"))
-  
+
 #   result_dicts = []
 #   for row in result.all():
 #     result_dicts.append(row._asdict())
-  
+
 #   print(result_dicts)
 
+# # print("result: ", result)
+# # print("type(result): ", type(result))
 
+# result_all = result.all()
+# # print("result.all(): ", result_all)
+# # print("type(result.all()): ", type(result_all))
 
+# first_result = result_all[0]
+# # print("first_result: ", first_result)
 
+# print("type(first_result): ", type(first_result))
+# # #### output == type(first_result):  <class 'sqlalchemy.engine.row.Row'>
 
+# # #### dict(first_result) and first_result.__dict__ did not worked
 
+# first_result_dict = first_result._asdict()
+# print("type(first_result_dict): ", type(first_result_dict))
+# # #### output == type(first_result_dict):  <class 'dict'>
 
-
-
-
-
-  # # print("result: ", result)
-  # # print("type(result): ", type(result))
-  
-  # result_all = result.all()
-  # # print("result.all(): ", result_all)
-  # # print("type(result.all()): ", type(result_all))
-  
-  # first_result = result_all[0]
-  # # print("first_result: ", first_result)
-  
-  # print("type(first_result): ", type(first_result))
-  # # #### output == type(first_result):  <class 'sqlalchemy.engine.row.Row'>
-  
-  # # #### dict(first_result) and first_result.__dict__ did not worked 
-
-  # first_result_dict = first_result._asdict()
-  # print("type(first_result_dict): ", type(first_result_dict))
-  # # #### output == type(first_result_dict):  <class 'dict'>
-
-  # # print("first_result_dict: ", first_result_dict)
+# # print("first_result_dict: ", first_result_dict)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # print("Hello world")
 # print(__name__)
 
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 # JOBS = [
 #   {
@@ -31,26 +31,27 @@ from database import load_jobs_from_db, load_job_from_db
 #   }
 # ]
 
-
-
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
   return render_template('home.html', jobs=jobs, company_name='Niharika')
-  #  ##### jobs=jobs -->  first_jobs=second_jobs --> first_jobs = HTML, second_jobs = return's function load_jobs_from_db() in main.app 
+  #  ##### jobs=jobs -->  first_jobs=second_jobs --> first_jobs = HTML, second_jobs = return's function load_jobs_from_db() in main.app
+
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+
 # @app.route("/job/<id>")
 # def show_job(id):
 #   job = load_job_from_db(id)
 #   return(jsonify(job))
+
 
 @app.route("/job/<id>")
 def show_job(id):
@@ -58,6 +59,21 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  # data = request.args ## when you post data, data is present in request.form
+  # store this in db # send an email # display an acknowledgement
+  ### return jsonify(data)
+  job = load_job_from_db(id)
+
+  add_application_to_db(id, data)
+
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == '__main__':
